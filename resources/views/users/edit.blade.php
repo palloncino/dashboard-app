f0a33da437f701f1cee129cd2fb8abbb6fe96827
palloncino <antonio.guiotto.dev@gmail.com>
2024-05-21 07:26:15 +0200

    progress

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -5,34 +5,34 @@
 @section('content')
     <div class="container mt-4">
         <h1>Edit User</h1>
-        <form action="{{ route('users.update', ->id) }}" method="POST">
+        <form action="{{ route('users.update', $user->id) }}" method="POST">
             @csrf
             @method('PUT')
             <div class="mb-3">
                 <label for="username" class="form-label">Username</label>
-                <input type="text" class="form-control" id="username" name="username" value="{{ ->username }}" required>
+                <input type="text" class="form-control" id="username" name="username" value="{{ $user->username }}" required>
             </div>
             <div class="mb-3">
                 <label for="firstName" class="form-label">First Name</label>
-                <input type="text" class="form-control" id="firstName" name="firstName" value="{{ ->firstName }}" required>
+                <input type="text" class="form-control" id="firstName" name="firstName" value="{{ $user->firstName }}" required>
             </div>
             <div class="mb-3">
                 <label for="lastName" class="form-label">Last Name</label>
-                <input type="text" class="form-control" id="lastName" name="lastName" value="{{ ->lastName }}" required>
+                <input type="text" class="form-control" id="lastName" name="lastName" value="{{ $user->lastName }}" required>
             </div>
             <div class="mb-3">
                 <label for="companyName" class="form-label">Company Name</label>
-                <input type="text" class="form-control" id="companyName" name="companyName" value="{{ ->companyName }}">
+                <input type="text" class="form-control" id="companyName" name="companyName" value="{{ $user->companyName }}">
             </div>
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" id="email" name="email" value="{{ ->email }}" required>
+                <input type="email" class="form-control" id="email" name="email" value="{{ $user->email }}" required>
             </div>
             <div class="mb-3">
                 <label for="role" class="form-label">Role</label>
-                <input type="text" class="form-control" id="role" name="role" value="{{ ->role }}" required>
+                <input type="text" class="form-control" id="role" name="role" value="{{ $user->role }}" required>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">Update User</button>
         </form>
     </div>
 @endsection
